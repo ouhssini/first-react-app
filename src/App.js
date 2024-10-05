4363638306c5b5322140939f66cad291c3a95493
Ouhssini <ahmedouhssini@gmail.com>
2024-10-05 23:39:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Product from "./components/Product";
+import { data } from "./data";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container px-4 mx-auto mt-5 flex flex-wrap  gap-4 justify-between">
+        {data.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+export const data = [
+    {
+      id: 1,
+      title: "PC Portable Gamer HP VICTUS",
+      price: "7490 DH",
+      rating : '4.8',
+      thumbnail: "images/HP16D0195NF.png",
+    },
+    {
+      id: 2,
+      title: "PC Portable Gamer HP VICTUS",
+      price: "2190 DH",
+      rating : '5.0',
+      thumbnail: "images/HP14424U3EA.png",
+    },
+    {
+      id: 3,
+      title: "Pc Portable Chromebook Acer",
+      price: "3640 DH",
+      rating : '3.9',
+      thumbnail: "images/NXATHEF002.png",
+    },
+    {
+      id: 4,
+      title: "PC Portable - HUAWEI",
+      price: "1270 DH",
+      rating : '4.5',
+      thumbnail: "images/HUA6901443442959.png",
+    },
+  ];
+  
